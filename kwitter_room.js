3d7c0b9ddf364607857086051f2c9d6416b7a326
IntermidateCoder <88549450+IntermidateCoder@users.noreply.github.com>
2022-08-06 18:01:00 +0400

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -33,7 +33,7 @@ function getData() {
             Room_names = childKey;
             //Start code
             console.log("Room Name - " + Room_names);
-            row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoom(this.id)' >#" + Room_names + "</div> <hr>";
+            row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoom(this.id)' >" + Room_names + "</div> <hr>";
             document.getElementById("output").innerHTML += row;
             //End code
         });
